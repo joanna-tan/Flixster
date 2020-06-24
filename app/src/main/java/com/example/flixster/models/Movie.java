e7b07673f39a793fbe8130ea33866f1466eebeed
Joanna Tan <joannatan@fb.com>
2020-06-24 14:42:49 -0400

    Added functionality to parse JSON objects

---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,46 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//java object to encapsulate Movie
+public class Movie {
+    String posterPath;
+    String title;
+    String overview;
+
+    //hard coded for size
+    //make API request to configurations, figure out sizes of images available, append to base URL
+        //then append poster path
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    // if fail, call a method to handle the exception
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    //creates list of movies from a JSON array from the Movie API
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+}
